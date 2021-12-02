276559015eeae86ddf4a5dad539d15b0ff867563
Claudia Reynders <>
2021-12-02 17:57:51 +0100

    Completed Day 2
    - added 2nd part of the solution

--- day2.py
+++ day2.py
@@ -1,20 +1,29 @@
 def dive():
+    testCourse = ["forward 5","down 5","forward 8","up 3","down 8","forward 2"]
+
     file = open("data/plannedcourse.txt")
     course = file.read().split("\n")
     file.close()
 
     xPos = int(0)
     depth = int(0)
+    aim = int(0)
 
     for pos in course:
         direction, movement = pos.split()
 
         if direction == "forward":
-            xPos += int(movement)
+            val = int(movement)
+            xPos += val
+            
+            if aim > 0 :
+                depth += (val * aim)
+            
         elif direction == "down":
-            depth += int(movement)
+            aim += int(movement)
+
         elif direction == "up":
-            depth -= int(movement)
+            aim -= int(movement)
 
     return xPos * depth
 
